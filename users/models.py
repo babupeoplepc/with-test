2e5fa7987965d8d134177f933d1efd09092c3991
babupeoplepc <babupeople.pocu@gmail.com>
2021-12-24 17:50:51 +0900

    index 작업중

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    student_id = models.CharField(max_length=10)
+
+
+# Create your models here.
