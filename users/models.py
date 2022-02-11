1ca0a83240121e7c3b332d79ea9ae06d2d0c81a2
babupeoplepc <babupeople.pocu@gmail.com>
2022-02-11 17:29:26 +0900

    수강생 회원가입 css html js 끝

--- users/models.py
+++ users/models.py
@@ -3,7 +3,23 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    student_id = models.CharField(max_length=10)
+    student_name = models.CharField(max_length=10)
+    student_gender = models.CharField(max_length=2)
+    student_email = models.CharField(max_length=100)
+    student_phone_number = models.CharField(max_length=20)
+    student_birth_year = models.IntegerField(default=0)
+    student_birth_month = models.IntegerField(default=0)
+    student_birth_day = models.IntegerField(default=0)
+    student_home_address = models.CharField(max_length=100)
+    student_job = models.CharField(max_length=20, blank=True)
+    student_visit = models.CharField(max_length=20, blank=True)
+    student_visit_example = models.TextField(blank=True)
+    student_comment = models.TextField(blank=True)
+    student_lesson_comment = models.TextField(blank=True)
+    student_push_stop = models.BooleanField(default=False)
+    
 
 
-# Create your models here.
+
+
+
--- users/views.py
+++ users/views.py
@@ -52,19 +52,43 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        firstname = request.POST["firstname"]
-        lastname = request.POST["lastname"]
-        email = request.POST["email"]
-        student_id = request.POST["student_id"]
-
-        user = User.objects.create_user(username, email, password)
-        user.last_name = lastname
-        user.firstname = firstname
-        user.student_id = student_id
-        user.save()
+        
         print(request.POST)
+        
+        username = request.POST["student_email"]
+        password = request.POST["student_password"]
+        email = request.POST["student_email"]
+        user = User.objects.create_user(username, email, password)
+
+        user.student_name = request.POST["student_name"]
+        user.student_gender = request.POST["student_gender"]
+        user.student_email = request.POST["student_email"]
+        user.student_phone_number = request.POST["student_phone_number"]
+        user.student_birth_year = request.POST["student_birth_year"]
+        user.student_birth_month = request.POST["student_birth_month"]
+        user.student_birth_day = request.POST["student_birth_day"]
+        user.student_home_address = request.POST["student_home_address"]
+        user.student_job = request.POST["student_job"]
+        user.student_visit = request.POST["student_visit"]
+        user.student_visit_example = request.POST["student_visit_example"]
+        user.student_comment = request.POST["student_comment"]
+        user.student_lesson_comment = request.POST["student_lesson_comment"]
+        
+        if 'student_push_stop' in request.POST:
+            print("on")
+            if request.POST["student_push_stop"] == "on":
+                user.student_push_stop = "on"
+
+        
+        user.save()
+        
+             
+            
+        
+
+        
+
+        
         return redirect("user:login")
     return render(request, "users/signup.html")
     
